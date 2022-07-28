31f7d9e177fff95b4410649c945c9d881390ffc1
Kafi Ashkir <82185473+19133@users.noreply.github.com>
2022-07-28 10:24:32 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,33 @@
+# Ask user what their name is
+
+# Figure out the user's budget
+
+# Show user a list of items and ask the user what item they would like
+
+# Ask user how much they would like their desired item to weigh
+
+# Ask user how many mililitres they would like their item to have (if they choose a drink)
+
+# Tell the user how much money it would cost to purchase that item with their desired increments
+
+# Ask the user if they would like extra sugar, fat, or calories. If they say yes, add extra money to the cost 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# Recommend what item the user should buy depending on how much money they have
+
